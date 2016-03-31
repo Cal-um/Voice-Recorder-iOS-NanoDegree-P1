59a71f73be1aaf04f53172b65ab20cc2d849ec9a
Calum Harris <harris.calum@gmail.com>
2016-03-31 23:48:42 +0100

    add vc

---
+++ Voice Transformer/PlaySoundsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlaySoundsViewController.swift
+//  Voice Transformer
+//
+//  Created by Calum Harris on 29/03/2016.
+//  Copyright © 2016 Calum Harris. All rights reserved.
+//
+
+import UIKit
+
+
+class PlaySoundsViewController: UIViewController {
+  
+  var recordedAudioURL: NSURL!
+  
+  
+  
+}
