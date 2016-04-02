db5914009be9334da1ea156ffc71a5f9b18ebd13
Calum Harris <harris.calum@gmail.com>
2016-04-02 17:26:46 +0100

    added sound effects

--- Voice Transformer/PlaySoundsViewController.swift
+++ Voice Transformer/PlaySoundsViewController.swift
@@ -12,6 +12,10 @@ import AVFoundation
 class PlaySoundsViewController: UIViewController {
   
   var recordedAudioURL: NSURL!
+  var audioFile: AVAudioFile!
+  var audioEngine: AVAudioEngine!
+  var audioPlayerNode: AVAudioPlayerNode!
+  var stopTimer: NSTimer!
   
   @IBOutlet weak var rabbitButton: UIButton!
   @IBOutlet weak var snailButton: UIButton!
@@ -21,19 +25,45 @@ class PlaySoundsViewController: UIViewController {
   @IBOutlet weak var parrotButton: UIButton!
   @IBOutlet weak var stopButton: UIButton!
   
+  enum ButtonType: Int { case Rabbit, Snail, Vader, Squirrel, Reverb, Parrot }
+  
+  
   @IBAction func playSound(sender: AnyObject) {
     
+    switch(ButtonType(rawValue: sender.tag)!) {
+      
+    case .Rabbit:
+      playSound(rate: 1.5)
+    case .Snail:
+      playSound(rate: 0.5)
+    case .Vader:
+      playSound(pitch: -1000)
+    case .Squirrel:
+      playSound(pitch: 1000)
+    case .Reverb:
+      playSound(reverb: true)
+    case .Parrot:
+      playSound(echo: true)
+    }
     
+    configureUI(.Playing)
     
   }
   
   @IBAction func stopSound(sender: AnyObject) {
-    
+    stopAudio()
     
     
   }
   
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupAudio()
+  }
   
+  override func viewWillAppear(animated: Bool) {
+    configureUI(.NotPlaying)
+  }
   
   
 }
